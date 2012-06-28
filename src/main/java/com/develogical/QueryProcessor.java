0953784030a63a9e98eb24d0772542d83c10c974
Mike McClurg <mike.mcclurg@gmail.com>
2012-06-28 14:56:48 +0100

    Split request, and use full "what is your name".

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -3,10 +3,19 @@ package com.develogical;
 public class QueryProcessor {
 
     public String process(String query) {
+        String[] request = query.split(":");
+
+        if (request.length == 1)
+            return processQuery(request[0]);
+        else
+            return processQuery(request[1]);
+    }
+
+    private String processQuery(String query) {
         if (query.contains("hi")) {
             return "hello";
         }
-        if (query.contains("name")) {
+        if (query.contains("what is your name")) {
             return "Uakari Ninjas";
         }
         System.out.println("Request was: " + query);
