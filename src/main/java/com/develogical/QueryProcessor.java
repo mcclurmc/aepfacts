eb51a0421d12fb2496b67f7e51001177b6a0037f
Stuart Leneghan <email@leneghan.com>
2012-06-28 15:35:53 +0100

    prime minister

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -15,6 +15,9 @@ public class QueryProcessor {
         if (query.contains("which of the following numbers is the largest")) {
             return new LargestNumber().findLargestNumber(request[2]);
         }
+        if (query.contains("who is the Prime Minister of Great Britain")) {
+            return "David Cameron";
+        }
         if (query.contains("what is your name")) {
             return "Uakari Ninjas";
         }
